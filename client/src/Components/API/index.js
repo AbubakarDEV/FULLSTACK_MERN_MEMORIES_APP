bca62dad5ff1ed78466dd379d4e96ed2af08960b
Abubakar Jilani <abub1156@gmail.com>
2023-09-28 15:57:56 +0500

    ffix url issue

--- client/src/Components/API/index.js
+++ client/src/Components/API/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const url = "https://breakable-overalls-goat.cyclic.cloud";
+const API = axios.create({ baseURL: url });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
