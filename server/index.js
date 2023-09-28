eec76fe585e6f7e2ae0374b416e9da31e854c64a
Abubakar Jilani <abub1156@gmail.com>
2023-09-28 15:18:33 +0500

    add port

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`Server Running on Port: http://localhost:${PORT}`)
